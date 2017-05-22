a48b475e2652d08c7ce7d9e2e72e847a6afdaa01
mdmunir <misbahuldmunir@gmail.com>
2017-05-22 15:06:42 +0700

    oauth

--- protected/views/user/login.php
+++ protected/views/user/login.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
@@ -17,16 +18,23 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['user/request-password-reset']) ?>.
-                    For new user you can <?= Html::a('signup', ['user/signup']) ?>.
-                </div>
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+            <?= $form->field($model, 'username') ?>
+            <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <div style="color:#999;margin:1em 0">
+                If you forgot your password you can <?= Html::a('reset it', ['user/request-password-reset']) ?>.
+                For new user you can <?= Html::a('signup', ['user/signup']) ?>.
+            </div>
+            <div class="form-group">
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
+            <div class="form-group">
+                <?=
+                yii\authclient\widgets\AuthChoice::widget([
+                    'baseAuthUrl' => ['site/auth']
+                ]);
+                ?>
+            </div>
             <?php ActiveForm::end(); ?>
         </div>
     </div>
